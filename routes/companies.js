529c8de9e960551a86d994288c1a0d3d419a6bb3
Feras Jobeir <fjobeir@gmail.com>
2023-01-09 15:45:17 +0300

    isAuthorized

--- routes/companies.js
+++ routes/companies.js
@@ -8,11 +8,25 @@ var {
   destroy,
   show,
 } = require("../controllers/companyController");
+const isAuthenticated = require("../middlewares/isAuthenticated");
+const isAuthorized = require("../middlewares/isAuthorized");
 
 router.post("/register", store);
 router.post("/login", login);
 router.get("/", index);
-router.put("/:id", update);
+router.put("/:id",
+  isAuthenticated,
+  function (req, res, next) {
+    isAuthorized(req, res, next, {
+      admin: {
+        matchId: false
+      },
+      company: {
+        matchId: true
+      }
+    })
+  },
+  update);
 router.delete("/:id", destroy);
 router.get("/:id", show);
 
